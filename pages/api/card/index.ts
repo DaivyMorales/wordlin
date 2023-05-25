8b226ebfe79f4c9e111e54d81c37f1167c2b4209
Daivy Morales <daivymorales7705@gmail.com>
2023-05-25 16:02:51 -0500

    Create end-point's card,  get and post

---
+++ pages/api/card/index.ts
@@ -0,0 +1,49 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { dbConnect } from "../../../utils/db";
+import Card from "../../../models/card.model";
+
+export default async function indexUser(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await dbConnect();
+
+  const { method, body } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const users = await Card.find();
+        return res.status(200).json(users);
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(500).json({ error: error.message });
+        } else {
+          res.status(500).json({ error: "Ha ocurrido un error." });
+        }
+      }
+      break;
+
+    case "POST":
+      try {
+        const { wordOne, wordTwo } = body;
+
+        const newCard = new Card({ wordOne, wordTwo });
+
+        const cardSaved = await newCard.save();
+
+        return res.status(200).json(cardSaved);
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(500).json({ error: error.message });
+        } else {
+          res.status(500).json({ error: "Ha ocurrido un error." });
+        }
+      }
+      break;
+
+    default:
+      res.status(400).json("Invalid method!");
+      break;
+  }
+}
