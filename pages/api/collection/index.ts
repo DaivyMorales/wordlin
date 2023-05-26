3a88c5f88a016af2c1009e58c5b2d3d98dab9c8e
Daivy Morales <daivymorales7705@gmail.com>
2023-05-26 08:08:49 -0500

    Add Collection's  end-points get and posst

---
+++ pages/api/collection/index.ts
@@ -0,0 +1,82 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { dbConnect } from "../../../utils/db";
+import Collection from "../../../models/collection.model";
+import mongoose from "mongoose";
+
+interface TCollection extends Document {
+  name: string;
+  cards: mongoose.Types.ObjectId[];
+}
+
+export default async function indexUser(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await dbConnect();
+
+  const { method, body } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const collections = await Collection.find().populate("cards");
+        return res.status(200).json(collections);
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(500).json({ error: error.message });
+        } else {
+          res.status(500).json({ error: "Ha ocurrido un error." });
+        }
+      }
+      break;
+
+    case "POST":
+      try {
+        const { name, cards }: { name: string; cards: string[] } = body;
+
+        const newCollection = new Collection({
+          name,
+          cards: cards.map((cardId) => new mongoose.Types.ObjectId(cardId)),
+        });
+
+        const collectionSaved: TCollection = await newCollection.save();
+
+        return res.status(200).json(collectionSaved);
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(500).json({ error: error.message });
+        } else {
+          res.status(500).json({ error: "Ha ocurrido un error." });
+        }
+      }
+      break;
+
+    default:
+      res.status(400).json("Invalid method!");
+      break;
+  }
+}
+
+/*
+{
+    "name": "Infinitive Words",
+    "cards":  [
+        {
+            _id: "6470a74ab7330683f3c1a895",
+            wordOne: "Write",
+            wordTwo: "Escribir"
+        },
+        {
+            _id: "6470a742b7330683f3c1a893",
+            wordOne: "Send",
+            wordTwo: "Enviar"
+        },
+        {
+            _id: "6470a73cb7330683f3c1a891",
+            wordOne: "Run",
+            wordTwo: "Correr"
+        }
+    ]
+}
+
+*/
